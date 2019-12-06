c977719f288f4a5a5e8784907a982bef79f89a85
udithindrakantha <udith.indrakantha@gmail.com>
2019-12-06 22:41:42 +0530

    Template Changed Totally

--- ImUdith/e2e/src/app.e2e-spec.ts
+++ ImUdith/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ImUdith!');
+    expect(page.getTitleText()).toEqual('personal-portfoli-angular app is running!');
   });
 
   afterEach(async () => {
--- ImUdith/src/app/app.component.spec.ts
+++ ImUdith/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ImUdith'`, () => {
+  it(`should have as title 'personal-portfoli-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ImUdith');
+    expect(app.title).toEqual('personal-portfoli-angular');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ImUdith!');
+    expect(compiled.querySelector('.content span').textContent).toContain('personal-portfoli-angular app is running!');
   });
 });
