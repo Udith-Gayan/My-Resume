a6f30d646544d26b6ef92bd2824f9e23c4b3d832
Udith-Gayan <udith.indrakantha@gmail.com>
2021-11-23 17:48:27 +0530

    Location details fetching set up

--- ImUdith/src/app/profile/intro/intro.component.ts
+++ ImUdith/src/app/profile/intro/intro.component.ts
@@ -9,6 +9,17 @@ import { environment } from '../../../environments/environment';
 })
 export class IntroComponent implements OnInit {
 
+  ipDetails = {
+   ipaddress: '',
+   latitude: '',
+   longitude:'',
+   currency:'',
+   currencysymbol:'',
+   isp: '',
+   city:'',
+   country:''
+  }
+
   visitor_count: number = 0;
   cvPath='https://drive.google.com/uc?export=download&id=';
 
@@ -24,6 +35,18 @@ export class IntroComponent implements OnInit {
     
     this.cvPath = this.cvPath + environment.assetsConfig.googleDriveAssets.myCVFileID;
 
+    this.counterService.getIpAddress().subscribe(res => {
+      this.ipDetails.ipaddress = res['ip'];
+      this.counterService.getGEOLocation(this.ipDetails.ipaddress).subscribe(res => {
+          this.ipDetails.latitude = res['latitude'];
+          this.ipDetails.longitude = res['longitude'];
+          this.ipDetails.currency = res['currency']['code'];
+          this.ipDetails.currencysymbol = res['currency']['symbol'];
+          this.ipDetails.city = res['city'];
+          this.ipDetails.country = res['country_code3'];
+          this.ipDetails.isp = res['isp'];
+          console.log(res);
+      });
+    });
   }
-
 }
--- ImUdith/src/app/services/firestore/visitor-tracker.service.ts
+++ ImUdith/src/app/services/firestore/visitor-tracker.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { environment } from '../../../environments/environment';
+import { HttpClient, HttpErrorResponse,HttpHeaders } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,7 @@ export class VisitorTrackerService {
   sharedDate = this.last_visited_date.asObservable();
 
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private http: HttpClient) {
   }
 
   getVisitorCount(): Observable<number>{
@@ -59,6 +61,43 @@ export class VisitorTrackerService {
                     });
     }
   }
+
+
+  getIpAddress() {
+    return this.http
+          .get('https://api.ipify.org/?format=json')
+          .pipe(
+            catchError(this.handleError)
+          );
+  }
+
+  getGEOLocation(ip) {
+    // Update your api key to get from https://ipgeolocation.io
+    let url = "https://api.ipgeolocation.io/ipgeo?apiKey=b3a90a211b56453193b1f1a8180288ed&ip="+ip; 
+      return this.http
+            .get(url)
+            .pipe(
+              catchError(this.handleError)
+            );
+  } 
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
+
 }
 
 
